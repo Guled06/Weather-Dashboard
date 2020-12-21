d6a69530c16a6abfdd9d9e59a77c879528659f09
Guled Ahmed <akindnesstoremember@gmail.com>
2020-12-21 14:36:27 -0800

    making changes in the index.html. rearranged the submit button from the top of the page to the bottom of the page

--- script.js
+++ script.js
@@ -5,18 +5,6 @@ $(function () {
   
   function fetchWeatherForCity(city) {
 
-    // var queryUrl = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q=" + city + "&units=imperial&appid=" + apiKey;
-
-
-    var queryUrl =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&units=imperial&appid=" +
-      apiKey;
-
-    $(".weather-card").addClass("card-loading");
-
-  
     var queryUrl =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       city +
@@ -30,12 +18,8 @@ $(function () {
       
       console.log(data);
 
-     
       $("#city-name").text(data.name + " Weather");
 
-      
-
-    
       var iconUrl =
         "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
 
@@ -50,13 +34,28 @@ $(function () {
       $("#wind").text(data.wind.speed + " mph");
       $("#humidity").text(data.main.humidity + " %");
 
-      // $("#futureTemp").text(list.main.temp + "°");
-      // $("#futureWind").text(list.wind.speed + " mph");
-      // $("#futureHumidity").text(list.main.humidity + " %");
-
-      $(".weather-card").removeClass("card-loading");
     });
   }
+
+
+  function fetchForecastForCity (city){
+
+    var queryUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    search +
+    "&units=imperial&appid=" +
+    apiKey;
+    
+    $.ajax({
+      url: queryUrl,
+      method: "GET",
+    }).then(function (data) {
+
+      console.log(data);
+
+    });
+  }
+
 
   $(document).on("click", ".city", function () {
     
@@ -77,7 +76,5 @@ $(function () {
 
     fetchWeatherForCity(city);
   });
-
-  
 });
 
